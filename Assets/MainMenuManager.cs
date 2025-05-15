c6292e1b8afc257031af751b1ac37298ff5bc2e4
heyitsjorge <jguzma60@calpoly.edu>
2025-05-14 17:37:37 -0700

    menu buttons

--- Assets/MainMenuManager.cs
+++ Assets/MainMenuManager.cs
@@ -6,11 +6,13 @@ public class MainMenuManager : MonoBehaviour
 {
     public Button startButton;
     public Button exitButton;
+    public Button creditButton;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         startButton.onClick.AddListener(StartGame);
         exitButton.onClick.AddListener(QuitGame);
+        creditButton.onClick.AddListener(ShowCredits);
     }
 
     public void StartGame()
@@ -24,8 +26,20 @@ private System.Collections.IEnumerator DelayedIntro()
     SceneManager.LoadScene("IntroCutScene");
 }
 
-    public void QuitGame()
-    {
+private System.Collections.IEnumerator DelayedCredits()
+{
+    yield return new WaitForSeconds(1.2f); // Wait for audio to finish
+    SceneManager.LoadScene("Credit Scene");
+}
+
+
+private void ShowCredits()
+{
+    StartCoroutine(DelayedCredits());
+}
+
+public void QuitGame()
+{
         // Quit the application
         Application.Quit();
 
@@ -33,11 +47,5 @@ private System.Collections.IEnumerator DelayedIntro()
         #if UNITY_EDITOR
         UnityEditor.EditorApplication.isPlaying = false;
         #endif
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+}
 }
